29b592e56bbad01ed9697fef19ed246b70bf47e1
Ben Gao <xingbingao@gmail.com>
2015-11-22 00:20:51 +1300

    Make it work

--- www/js/HomeController.js
+++
@@ -1,6 +0,0 @@
-angular.module('FashionGhost.controllers', [])
-	.controller('HomeController', function($scope, $state, $rootScope) {
-		console.log('home');
-		
-		// $state.go('welcome');
-	})
--- www/js/app.js
+++ www/js/app.js
@@ -1,35 +1,86 @@
-angular.module('FashionGhost', ['ionic', 'FashionGhost.controllers'])
-  .config(function($stateProvider, $urlRouterProvider) {
-    $stateProvider
-      .state('app', {
-        url: '/app',
-        abstract: true,
-        templateUrl: 'templates/app.html'
-      })
-      .state('app.home', {
-        url: '/home',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/home.html',
-            controller: 'HomeController'
-          }
-        }
-      })
+// Ionic Parse Starter App
 
-    $urlRouterProvider.otherwise('/')
-  })
-  .run(function($ionicPlatform, $state, $rootScope) {
-    $ionicPlatform.ready(function() {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      if (window.cordova && window.cordova.plugins.Keyboard) {
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true)
-      }
-      if (window.StatusBar) {
-        StatusBar.styleDefault()
-      }
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'ionicParseApp' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'ionicParseApp.controllers' is found in controllers.js
+angular.module('ionicParseApp',
+        [ 'ionic', 'ionicParseApp.controllers' ]
+    )
+    .config(function($stateProvider, $urlRouterProvider) {
 
-      Parse.initialize('qHDj4x6Wc9qqp4kChk9u2pTURr2EQdh5aaMMYU3W', 'f5h0XtsTnEXmtPRMvTv47ndZD8xSSPuhNhjjAToc')
-      $state.go('app.home');
+        // Ionic uses AngularUI Router which uses the concept of states
+        // Learn more here: https://github.com/angular-ui/ui-router
+        // Set up the various states which the app can be in.
+        // Each state's controller can be found in controllers.js
+        $stateProvider
+
+            // setup an abstract state for the tabs directive
+            .state('welcome', {
+                url: '/welcome?clear',
+                templateUrl: 'templates/welcome.html',
+                controller: 'WelcomeController'
+            })
+
+            .state('app', {
+                url: '/app?clear',
+                abstract: true,
+                templateUrl: 'templates/menu.html',
+                controller: 'AppController'
+            })
+
+            .state('app.home', {
+                url: '/home',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/home.html',
+                        controller: 'HomeController'
+                    }
+                }
+            })
+
+            .state('app.login', {
+                url: '/login',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/login.html',
+                        controller: 'LoginController'
+                    }
+                }
+            })
+
+            .state('app.forgot', {
+                url: '/forgot',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/forgotPassword.html',
+                        controller: 'ForgotPasswordController'
+                    }
+                }
+            })
+
+            .state('app.register', {
+                url: '/register',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/register.html',
+                        controller: 'RegisterController'
+                    }
+                }
+            });
+
+        $urlRouterProvider.otherwise('/welcome');
     })
-  })
+    .run(function ($state, $rootScope) {
+        Parse.initialize('applicationId', 'javascriptKey');
+        var currentUser = Parse.User.current();
+        $rootScope.user = null;
+        $rootScope.isLoggedIn = false;
+
+        if (currentUser) {
+            $rootScope.user = currentUser;
+            $rootScope.isLoggedIn = true;
+            $state.go('app.home');
+        }
+    });
+
