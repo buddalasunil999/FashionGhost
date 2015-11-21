b68c4706ac13e8e643a4b74f52ec589b15c93373
Ben Gao <xingbingao@gmail.com>
2015-11-21 21:54:04 +1300

    Routes and controllers

---
+++ www/js/HomeController.js
@@ -0,0 +1,6 @@
+angular.module('FashionGhost.controllers', [])
+	.controller('HomeController', function($scope, $state, $rootScope) {
+		console.log('home');
+		
+		// $state.go('welcome');
+	})
--- www/js/app.js
+++ www/js/app.js
@@ -1,12 +1,25 @@
-// Ionic Starter App
+angular.module('FashionGhost', ['ionic', 'FashionGhost.controllers'])
+  .config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/app.html'
+      })
+      .state('app.home', {
+        url: '/home',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/home.html',
+            controller: 'HomeController'
+          }
+        }
+      })
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'FashionGhost' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('FashionGhost', ['ionic'])
-
-  .run(function ($ionicPlatform) {
-    $ionicPlatform.ready(function () {
+    $urlRouterProvider.otherwise('/')
+  })
+  .run(function($ionicPlatform, $state, $rootScope) {
+    $ionicPlatform.ready(function() {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
       if (window.cordova && window.cordova.plugins.Keyboard) {
@@ -17,6 +30,6 @@ angular.module('FashionGhost', ['ionic'])
       }
 
       Parse.initialize('qHDj4x6Wc9qqp4kChk9u2pTURr2EQdh5aaMMYU3W', 'f5h0XtsTnEXmtPRMvTv47ndZD8xSSPuhNhjjAToc')
-
+      $state.go('app.home');
     })
   })
